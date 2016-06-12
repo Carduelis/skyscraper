0923e3f27c24d0f705dae6b1fa11b146ec39dfe6
Carduelis <pavepy@yandex.ru>
2016-06-12 19:57:14 +0300

    data from array

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+function setSymbolSize($el) {
+	console.log($el.val(),$el.val().length)
+	switch ($el.val().length) {
+		case 1: 
+			$el.attr('data-state','answer');
+			break;
+		case 2: 
+			$el.attr('data-state','guess');
+			break;
+		case 3: 
+			$el.attr('data-state','guess-three');
+			break;
+		default: 
+			$el.val($el.val().slice(0,3));
+			break;
+	}
+}
 $(document).ready(function() {
 	$puzzle = $('.puzzle');
 	$.ajax({
@@ -9,25 +26,25 @@ $(document).ready(function() {
 		for (var i = 0; i < data.size; i++) {
 			$puzzle.append('<div class="row" data-row="'+i+'">')
 			for (var j = 0; j < data.size; j++) {
+				var inputValue = data.default[i][j] ? data.default[i][j] : '';
 				$row = $puzzle.find('.row[data-row="'+i+'"]')
-				$row.append('<div class="cell"><input type="text" name="cell['+i+']['+j+']" class="form-control"></div>')
+				var html = '<div class="cell">';
+				html+=	'<input type="text" class="form-control" ';
+				console.log(inputValue)
+				if (inputValue) {
+					html+=	' data-state="default" readonly ';
+					html+= ' value="'+inputValue+'" ';
+				}
+				html+= 'name="cell['+i+']['+j+']"';
+				html+= ' > ';
+				html+= '</div>';
+
+				$row.append(html);
 			}
 
 		}
-		
 		$puzzle.on('input', 'input', function(){
-			console.log($(this).val(),$(this).val().length)
-			switch ($(this).val().length) {
-				case 1: 
-					$(this).attr('data-state','answer');
-					break;
-				case 2: 
-					$(this).attr('data-state','guess');
-					break;
-				default: 
-					$(this).val($(this).val().slice(0,2))
-					break;
-			}
+			setSymbolSize($(this));
 		})
 
 
